32e784f558a4deadb88ffadecd47abf3d30e6a35
Nam <lkunelk@gmail.com>
2019-11-14 18:47:31 -0500

    define structure of motionplan.m

---
+++ lab3/motionplan.m
@@ -0,0 +1,10 @@
+function motionplan(q0,q2,t1,t2,myrobot,obs,tol)
+    % q0: column vector of initial configuration angles
+    % q2: column vector of final joint angles
+    % t1, t2: start and end time of piecewise cubic polynomial (PWCP)
+    % myrobot: robot structure
+    % obs: obstacle structure
+    % tol: tolerance for terminating search
+    
+    clear q % (Nx6) matrix of angles
+end
