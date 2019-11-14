59107f41681aeb36b18f1a6816affb5bffaacbc5
Nam Nguyen <lkunelk@gmail.com>
2019-11-14 13:14:57 -0500

    Add files via upload
    add initial files

---
+++ lab3/att.m
@@ -0,0 +1,18 @@
+function tau = att(q, q2, myrobot)
+    % 
+    % q: column vector of current angles
+    % q2: columne vector of final angles
+    % myrobot: robot structure
+    % return tau: row vectors of torques on each joint
+    
+    % step1: compute forward kinematics of each origin of current angle
+    
+    % step2: compute forward kinematics of each origin of final angles
+    
+    % step3: compute artificial forces
+    
+    % step4: compute Jacobians
+    
+    % step5: compute torques
+    
+end
