8b5049f9700c649959fef7bf09e04942f785e35a
Nam <lkunelk@gmail.com>
2019-11-14 19:22:01 -0500

    implemented gradient descent untested

--- lab3/motionplan.m
+++ lab3/motionplan.m
@@ -1,4 +1,4 @@
-function motionplan(q0,q2,t1,t2,myrobot,obs,tol)
+function qref = motionplan(q0,q2,t1,t2,myrobot,obs,tol)
     % q0: column vector of initial configuration angles
     % q2: column vector of final joint angles
     % t1, t2: start and end time of piecewise cubic polynomial (PWCP)
@@ -7,4 +7,21 @@ function motionplan(q0,q2,t1,t2,myrobot,obs,tol)
     % tol: tolerance for terminating search
     
     clear q % (Nx6) matrix of angles
+    alpha = .1; % step size
+    q(1, :) = q0';
+    
+    % while
+    for i=1:10
+        % get torques from attractive and repulsice forces
+        %tau = att(q(end, :)', q2, myrobot)
+        tau = [2 2 2 2 1 1]
+        
+        % update angles
+        q(end+1, :) = q(end, :) + alpha * tau / norm(tau);
+    end
+    
+    % create PWCP based on q
+    % do it after testing
+    
+    qref = q;
 end
