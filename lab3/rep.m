c80d745cfff218d1166d4d210ff7268c0a214dac
Nam <lkunelk@gmail.com>
2019-11-15 15:46:02 -0500

    add skeleton for repulsive force function

---
+++ lab3/rep.m
@@ -0,0 +1,18 @@
+function rep(q, myrobot, obs)
+    % q: column vector of current joint angles
+    % myrobot: the robot
+    % obs: obstacles as defined in setupobstacles.m
+    % return torques for each joint 
+    
+    rho_0 = 10  % cm
+    % compute F repulsive for obstacle on each joint
+    
+    
+    % compute Jacobian
+    
+    % compute torques
+end
+
+function dist_cylinder(q, myrobot, obs)
+    % compute distances to cylinder for all joints
+end
