98644e1b0dbc36045f46657d161ab17594f8762d
Nam <lkunelk@gmail.com>
2019-11-15 18:20:07 -0500

    put gradient descent code in separate function

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,9 +1,27 @@
 function Lab3()
+    %test_plot()
+    test_motionplan()
+end
+
+function test_plot()
     my_robot = init_robot();
     q = sample_traj();
-    obs = setupobstacle();
-    plotobstacle(obs);
-    plot_robot(q, my_robot)
+    %obs = setupobstacle();
+    %plotobstacle(obs);
+    plot_robot(q, my_robot);
+end
+
+function test_motionplan()
+    myrobot = init_robot();
+    H1 = eul2tr([0 pi pi/2]); % eul2tr converts ZYZ Euler angles to a hom. tsf. mtx
+    H1(1:3,4)= [50; 50; 10;]; % This assigns the desired displacement to the hom.tsf.mtx.
+    q1 = inverse_puma(H1,myrobot);
+    H2 = eul2tr([0 pi pi/2]);
+    H2(1:3,4)=[50; 50; 50;];
+    q2 = inverse_puma(H2,myrobot);
+    
+    q = motionplan(q1', q2', 0, 0, myrobot, 0, 0.01);
+    plot_robot(q, myrobot)
 end
 
 function robot = init_robot()
@@ -36,6 +54,6 @@ function plot_robot(q, robot)
     H = forward(q,robot);
     m = H.transl;
     plot3(m(:,1),m(:,2),m(:,3),'r')
-    hold on % keep the previous plot
-    plot(robot,q)
+    
+    robot.plot(q, 'floorlevel', 0, 'workspace', [-100, 100, -100, 100, 0, 200])
 end
