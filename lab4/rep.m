fed41d4692167a7636f58e219fcb7d825db655b0
Nam <lkunelk@gmail.com>
2019-11-22 09:11:47 -0500

    fix indentation

--- lab4/rep.m
+++ lab4/rep.m
@@ -11,37 +11,37 @@ function tau = rep(q, myrobot, obs)
     Frep = zeros(3,6);
     for i = 1:6
         if (obs.type == 'cyl')                
-                % TODO: Arnav implement cylinder
-                % 3 cases:
-                %   - joint directly above cylinder
-                %   - joint directly to the side of cylinder (same as lab3)
-                %   - joint diagonally away from cylinder
-                
-                R = obs.R; % Radius of cylinder
-                C = obs.c; % Centre position of cylinder
-                rho = obs.rho0;
-                H = obs.h;
-                oc = Hs(1:2, 4, i);
-                if (Hs(3,4,i) <= H)
-                    dist = norm(C - oc);
-                    dir = [(oc - C)/norm(oc - obs.c); 0];
-                    shortest_dist = dist - obs.R;
-                elseif (norm(C - oc) > R)
-                     N = norm(oc - C);
-                     point = C - R*(C - oc)/N;
-                     dir = [(oc - point); (Hs(3,4,i) - H)];
-                     shortest_dist = norm(Hs(1:3,4,i) - [point;H]);
-                     dir = dir / norm(dir);
-                else
-                    shortest_dist = Hs(3,4,i) - H;
-                    dir = [0;0;1];
-                end
-                if shortest_dist < rho
-                        Frep(:, i) = Frep(:, i) + zeta*(1/shortest_dist - 1/rho)/shortest_dist^2 * dir;
-                end                
+            % TODO: Arnav implement cylinder
+            % 3 cases:
+            %   - joint directly above cylinder
+            %   - joint directly to the side of cylinder (same as lab3)
+            %   - joint diagonally away from cylinder
+
+            R = obs.R; % Radius of cylinder
+            C = obs.c; % Centre position of cylinder
+            rho = obs.rho0;
+            H = obs.h;
+            oc = Hs(1:2, 4, i);
+            if (Hs(3,4,i) <= H)
+                dist = norm(C - oc);
+                dir = [(oc - C)/norm(oc - C); 0];
+                shortest_dist = dist - R;
+            elseif (norm(C - oc) > R)
+                 N = norm(oc - C);
+                 point = C - R*(C - oc)/N;
+                 shortest_dist = norm(Hs(1:3,4,i) - [point;H]);
+                 dir = [(oc - point); (Hs(3,4,i) - H)];
+                 dir = dir / norm(dir);
+            else
+                shortest_dist = Hs(3,4,i) - H;
+                dir = [0;0;1];
+            end
+            if shortest_dist < rho
+                Frep(:, i) = Frep(:, i) + zeta*(1/shortest_dist - 1/rho)/shortest_dist^2 * dir;
+            end                
         elseif (obs.type == 'workspace')
-                H = obs.h;
-                % TODO: Nam implement force from workspace
+            H = obs.h;
+            % TODO: Nam implement force from workspace
         end
     end
     
